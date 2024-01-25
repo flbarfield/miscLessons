cde8501742f88997dcee96515339dfea3a51895e
Ricky Barfield <barfieldfredrick@gmail.com>
2024-01-24 21:22:25 -0700

    properly implemented partitioning and added quicksort

--- PythonDSACourses/common_sense_exercises/ch_13/partitioning_quicksort.py
+++ PythonDSACourses/common_sense_exercises/ch_13/partitioning_quicksort.py
@@ -5,14 +5,14 @@ class SortableArray:
     def __init__(self, array):
         self.array = array
 
-    def partition(self, array, left_pointer, right_pointer):
+    def partition(self, left_pointer, right_pointer):
         '''implementation of a partition'''
         # We always choose the right-most element as the pivot.
         # We keep the index of the pivot for later use:
         pivot_index = right_pointer
 
         # We grab the pivot value itself:
-        pivot = array[pivot_index]
+        pivot = self.array[pivot_index]
 
         # We start the right pointer immediately to the left of the
         # pivot
@@ -21,12 +21,12 @@ class SortableArray:
         while True:
             # Move the pointer to the right as long as it points
             # to value that is less than the pivot:
-            while array[left_pointer] < pivot:
+            while self.array[left_pointer] < pivot:
                 left_pointer += 1
 
             # Move the left pointer to the left as long as it points
             # to value that is greater than pivot
-            while array[right_pointer] > pivot:
+            while self.array[right_pointer] > pivot:
                 right_pointer -= 1
 
             # We've now reached the point where we've stopped moving
@@ -41,8 +41,8 @@ class SortableArray:
             # If the left pointer is still to the left of the right pointer, we
             # swap the values of the left and right pointers
             else:
-                array[left_pointer], array[right_pointer] = \
-                array[right_pointer], array[left_pointer]
+                self.array[left_pointer], self.array[right_pointer] = \
+                self.array[right_pointer], self.array[left_pointer]
 
             # We move the left pointer over to the right, gearing up for the next
             # round of left and right pointer movements:
@@ -50,8 +50,22 @@ class SortableArray:
 
     # As a final step of the partition, we swap the value of the left pointer
     # with the pivot:
-        array[left_pointer], array[pivot_index] = array[pivot_index], array[left_pointer]
+        self.array[left_pointer], self.array[pivot_index] = \
+        self.array[pivot_index], self.array[left_pointer]
 
     # We return the left_pointer for the sake of the quicksort method, which will appear
     # later in this chapter:
         return left_pointer
+
+    def quicksort(self, left_index, right_index):
+        '''Implementation of quicksort'''
+        # Base case: the subarray has 0 or 1 elements:
+        if right_index - left_index <= 0:
+            return
+
+        # Partition the range of elements and grab the index of the pivot:
+        pivot_index = self.partition(left_index, right_index)
+
+        # Recusively call this quicksort method on whatever is to the left
+        # of the pivot
+        self.quicksort(left_index, pivot_index - 1)
--- PythonDSACourses/test.py
+++
@@ -1,6 +0,0 @@
-def double_array(array, index=0):
-    if index >= len(array):
-        return
-    
-    array[index] *= 2
-    double_array(array, index + 1)
