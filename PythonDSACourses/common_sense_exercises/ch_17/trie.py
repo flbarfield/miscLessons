22a3d436f2db5b08479452d58cce35dd3b6ebff1
Ricky Barfield <barfieldfredrick@gmail.com>
2024-02-17 20:43:08 -0700

    implemented search inside of Trie

--- PythonDSACourses/common_sense_exercises/ch_17/trie.py
+++ PythonDSACourses/common_sense_exercises/ch_17/trie.py
@@ -9,3 +9,21 @@ class Trie:
     '''Implementation of a Trie'''
     def __init__(self):
         self.root = TrieNode()
+
+    def search(self, word):
+        '''Implementation of a search feature'''
+        current_node = self.root
+
+        for char in word:
+            # If the current node has child key with current
+            # character:
+            if current_node.children.get(char):
+                # Follow the child node:
+                current_node = current_node.children[char]
+            else:
+                # If the current character isn't found among the
+                # current node's children, our search word must
+                # not be in the trie:
+                return None
+
+        return current_node
